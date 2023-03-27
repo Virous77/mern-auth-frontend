ca70e0acf84cfd976f165cc39e2b80f8fba6f35f
Reetesh Kumar <rajreetesh7@gmail.com>
2023-03-28 00:16:13 +0530

    fixed url

--- src/components/api/authApi.jsx
+++ src/components/api/authApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-const API_URL = `${BACKEND_URL}/api-v1/users`;
+const API_URL = `${BACKEND_URL}api-v1/users`;
 
 export const register = async (userData) => {
   const { data } = await axios.post(`${API_URL}/register`, userData);
